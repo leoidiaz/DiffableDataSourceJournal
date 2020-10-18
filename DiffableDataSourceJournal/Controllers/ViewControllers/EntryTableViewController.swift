0acaee9e185783d7b077d9b4428e9dc8c1316787
leoidiaz <zaid.leonardo@gmail.com>
2020-10-17 18:25:07 -0700

    EntryController setup

--- DiffableDataSourceJournal/Controllers/ViewControllers/EntryTableViewController.swift
+++ DiffableDataSourceJournal/Controllers/ViewControllers/EntryTableViewController.swift
@@ -7,83 +7,63 @@
 
 import UIKit
 
-class EntryTableViewController: UITableViewController {
+fileprivate enum EntrySection {
+    case main
+}
 
+class EntryTableViewController: UITableViewController {
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        setupView()
+        configureDataSource()
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    var entries = [Entry]()
+    //MARK: - Properties
+    var journal: Journal? {
+        didSet{
+            guard let journal = journal else { return }
+            entries = journal.entries
+        }
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    
+    private var dataSource: UITableViewDiffableDataSource<EntrySection, Entry>!
+    
+    private func setupView(){
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "entryCell")
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(createEntry))
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    @objc func createEntry(){
+        guard let journal = journal else { return }
+        let detailsVC = DetailsViewController()
+        detailsVC.journal = journal
+        navigationController?.pushViewController(detailsVC, animated: true)
     }
-    */
+}
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    // MARK: - Table view diffable data source
+extension EntryTableViewController {
+    private func configureDataSource(){
+        dataSource = UITableViewDiffableDataSource<EntrySection, Entry>(tableView: tableView) { (tableView, indexPath, entry) -> UITableViewCell? in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "entryCell", for: indexPath)
+            cell.textLabel?.text = entry.title
+            cell.accessoryType = .disclosureIndicator
+            return cell
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    private func createSnapshot(from entry: [Entry]){
+        var snapShot = NSDiffableDataSourceSnapshot<EntrySection, Entry>()
+        snapShot.appendSections([.main])
+        snapShot.appendItems(entry)
+        dataSource.apply(snapShot, animatingDifferences: true)
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let entry = dataSource.itemIdentifier(for: indexPath) else { return }
+        let detailsVC = DetailsViewController()
+        detailsVC.entry = entry
+        navigationController?.pushViewController(detailsVC, animated: true)
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
