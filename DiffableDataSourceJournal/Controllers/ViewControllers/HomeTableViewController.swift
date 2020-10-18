a034447adfdb52cd4aa2bd52ceb57fdb6e7bcafb
leoidiaz <zaid.leonardo@gmail.com>
2020-10-17 18:25:37 -0700

    HomeTableViewController setup

--- DiffableDataSourceJournal/Controllers/ViewControllers/HomeTableViewController.swift
+++ DiffableDataSourceJournal/Controllers/ViewControllers/HomeTableViewController.swift
@@ -7,82 +7,61 @@
 
 import UIKit
 
-class HomeTableViewController: UITableViewController {
+fileprivate enum JournalSection {
+    case main
+}
 
+class HomeTableViewController: UITableViewController {
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        setupView()
+        configureDataSource()
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    //MARK: - Properties
+    private var dataSource: UITableViewDiffableDataSource<JournalSection, Journal>!
+    
+    //MARK: - Helper Methods
+    private func setupView(){
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "journalCell")
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showAlert))
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    
+    @objc private func showAlert(){
+        let alertController = UIAlertController(title: "New Journal", message: nil, preferredStyle: .alert)
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Name of Journal"
+        }
+        alertController.addAction(UIAlertAction(title: "Add", style: .default, handler: { [weak self] (_) in
+            guard let text = alertController.textFields?.first?.text, !text.isEmpty else { return }
+            JournalController.shared.createJournal(with: text)
+            self?.createSnapshot(from: JournalController.shared.journals)
+        }))
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(alertController, animated: true)
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+}
+    //MARK: - Table view Diffable data source
+extension HomeTableViewController {
+    private func configureDataSource(){
+        dataSource = UITableViewDiffableDataSource<JournalSection, Journal>(tableView: tableView) { (tableView, indexPath, journal) -> UITableViewCell? in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "journalCell", for: indexPath)
+            cell.textLabel?.text = journal.title
+            cell.accessoryType = .disclosureIndicator
+            return cell
+        }
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    private func createSnapshot(from journal: [Journal]){
+        var snapShot = NSDiffableDataSourceSnapshot<JournalSection, Journal>()
+        snapShot.appendSections([.main])
+        snapShot.appendItems(journal)
+        dataSource.apply(snapShot, animatingDifferences: true)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let journal = dataSource.itemIdentifier(for: indexPath) else { return }
+        let entryVC = EntryTableViewController()
+        entryVC.journal = journal
+        navigationController?.pushViewController(entryVC, animated: true)
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
