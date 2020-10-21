7811dcdea66a242fe4551ffe965650836f1531b9
leoidiaz <zaid.leonardo@gmail.com>
2020-10-21 12:55:46 -0700

    enum for potential error

--- DiffableDataSourceJournal/Helper/JournalError.swift
+++ DiffableDataSourceJournal/Helper/JournalError.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+enum JournalError: LocalizedError {
+    case thrownError(Error)
+    case emptyTextField
+    
+    var errorDescription: String? {
+        switch self {
+        case .thrownError(let error):
+            return error.localizedDescription
+        case .emptyTextField:
+            return "Textfield can not be left empty!"
+        }
+    }
+}
