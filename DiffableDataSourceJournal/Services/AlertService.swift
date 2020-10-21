aba0512c4c0784ae2786648d9dbf36d0d0b16ca8
leoidiaz <zaid.leonardo@gmail.com>
2020-10-21 12:56:26 -0700

    AlertService methods

--- DiffableDataSourceJournal/Services/AlertService.swift
+++ DiffableDataSourceJournal/Services/AlertService.swift
@@ -5,4 +5,27 @@
 //  Created by Leonardo Diaz on 10/21/20.
 //
 
-import Foundation
+import UIKit.UIAlertController
+
+
+struct AlertService {
+    func createJournalAlert(alertTitle: String?,  completion: @escaping (String) -> Void) -> UIAlertController {
+        let alertController = UIAlertController(title: alertTitle, message: nil, preferredStyle: .alert)
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Name of Journal"
+        }
+        alertController.addAction(UIAlertAction(title: "Add", style: .default, handler: { (_) in
+            guard let noteTitle = alertController.textFields?.first?.text, !noteTitle.isEmpty else { return }
+            completion(noteTitle)
+        }))
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        return alertController
+    }
+    
+    func presentErrorToUser(localizedError: JournalError, title: String?) -> UIAlertController {
+        let alertController = UIAlertController(title: title, message: localizedError.errorDescription, preferredStyle: .alert)
+        let dismissAction = UIAlertAction(title: "Ok", style: .cancel)
+        alertController.addAction(dismissAction)
+        return alertController
+    }
+}
